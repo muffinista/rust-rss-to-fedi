a0d1b9dbb9203f4ac97359f4bd4d8fa034d74907
Colin Mitchell <muffinista@gmail.com>
2022-12-25 15:55:23 -0500

    Drop bad reference

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,11 @@ pub mod feed;
 pub mod follower;
 pub mod item;
 pub mod mailer;
-pub mod webfinger_ext;
 
 pub use user::User;
 pub use feed::Feed;
 pub use follower::Follower;
 pub use item::Item;
-pub use webfinger_ext::WebfingerExtended;
 
 pub mod routes;
 pub mod server;
