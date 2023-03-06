4b9e1b41d9aa5c64df2b39d0ca7c30b5be3d2a75
muffinista <colin@muffinlabs.com>
2023-03-06 16:45:07 -0500

    Set log level for db queries

--- src/utils/pool.rs
+++ src/utils/pool.rs
@@ -1,8 +1,15 @@
 use std::env;
 use std::str::FromStr;
 
-use sqlx::postgres::PgPoolOptions;
-use sqlx::{Pool, Postgres};
+use sqlx::{
+  Pool,
+  Postgres,
+  ConnectOptions,
+  postgres::{PgPoolOptions, PgConnectOptions}
+};
+use std::time::Duration;
+use log::LevelFilter;
+
 
 const POOL_SIZE: u32 = 5;
 const WORKER_POOL_SIZE: u32 = 3;
@@ -26,22 +33,27 @@ pub fn worker_pool_size() -> u32 {
   }
 }
 
-pub async fn web_db_pool() -> Pool<Postgres> {
+fn connect_options() -> PgConnectOptions {
   let db_uri = env::var("DATABASE_URL").expect("DATABASE_URL is not set");
+  PgConnectOptions::from_str(&db_uri)
+    .expect("failed to parse DATABASE_URL")
+    .log_statements(LevelFilter::Warn)
+    .log_slow_statements(LevelFilter::Warn, Duration::from_millis(200))
+    .clone()
+}
 
+pub async fn web_db_pool() -> Pool<Postgres> {
   PgPoolOptions::new()
     .max_connections(web_pool_size())
-    .connect(&db_uri)
+    .connect_with(connect_options())
     .await
     .expect("Failed to create pool")
 }
 
 pub async fn worker_db_pool() -> Pool<Postgres> {
-  let db_uri = env::var("DATABASE_URL").expect("DATABASE_URL is not set");
-
   PgPoolOptions::new()
     .max_connections(worker_pool_size())
-    .connect(&db_uri)
+    .connect_with(connect_options())
     .await
     .expect("Failed to create pool")
 }
