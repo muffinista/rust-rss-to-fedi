23f694dda33681efb0905e138e3f9edcbc7f9dfb
muffinista <colin@muffinlabs.com>
2023-03-09 08:47:36 -0500

    Cleanup dialog. Use font size large enough to disable iOS auto zoom

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -10,9 +10,14 @@ rustup component add llvm-tools-preview
 export RUSTFLAGS="-Cinstrument-coverage"
 export LLVM_PROFILE_FILE="profile/rustypub-%p-%m.profraw"
 
+cargo +nightly test
+
+
+rm -rf target/
+
 scripts/test
 
-grcov profile/ -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
+grcov ../rust-activitypub/profile/ -s . --binary-path ../rust-activitypub/target/debug/ -t html --llvm --branch --ignore-not-existing -o ../rust-activitypub/target/debug/coverage/
 
 
 # https://vladfilippov.com/rust-code-coverage-tools/
