975544df69469d7ffc85281f6fc1f0b04a495bb4
muffinista <colin@muffinlabs.com>
2023-03-10 08:43:17 -0500

    Ensure page is loaded before JS

--- assets/script.js
+++ assets/script.js
@@ -1,92 +1,88 @@
-let button = document.querySelector(".add-feed button.check");
-if (button) {
-  const checkForValidFeed = async (event) => {
-    event.preventDefault();
+window.addEventListener("load", (event) => {
+  let button = document.querySelector(".add-feed button.check");
+  if (button) {
+    const checkForValidFeed = async (event) => {
+      event.preventDefault();
 
-    const messageDest = document.querySelector(".add-feed-results");
-    const name = document.querySelector(".add-feed input[name='name']").value;
-    const url = document.querySelector(".add-feed input[name='url']").value;
-    const payload = {
-      name, url
-    }
-
-    if (!name.match(/^[a-z0-9]/i) ) {
-      messageDest.innerHTML = "Sorry, please limit the username to letters and digits";
-      return;
-    }
-
-    messageDest.innerHTML = "Checking that feed is valid..."; 
-
-    const response = await fetch('/test-feed', {
-      method: 'POST',
-      body: JSON.stringify(payload),
-    });
-
-    if ( response.status == 404 ) {
-      console.log("No feed there");
-      messageDest.innerHTML = "Sorry, we couldn't find a valid feed at that URL";
-    } else if ( response.status > 200 ) {
-      console.log("Something went wrong");
-      messageDest.innerHTML = "Sorry, something went wrong";
-    } else {
-      const data = await response.json();
-      console.log(data);
-
-      if ( data.error ) {
-        messageDest.innerHTML = data.error;  
-      } else {
-        document.querySelector(".add-feed input[name='url']").value = data.url;
-        document.querySelector(".add-feed").attributes.action = "/feed";
-  
-        button.removeEventListener("click", checkForValidFeed);
-        button.type = "submit";
-  
-        messageDest.innerHTML = "Looks good! Click 'Add feed' one more time to create the account";  
+      const messageDest = document.querySelector(".add-feed-results");
+      const name = document.querySelector(".add-feed input[name='name']").value;
+      const url = document.querySelector(".add-feed input[name='url']").value;
+      const payload = {
+        name, url
       }
-    }
-  };
 
-  button.addEventListener("click", checkForValidFeed);
-}
+      if (!name.match(/^[a-z0-9]/i) ) {
+        messageDest.innerHTML = "Sorry, please limit the username to letters and digits";
+        return;
+      }
 
-function copy(target) {
-  target.querySelector(".copy-target").select();
-  document.execCommand("copy");
+      messageDest.innerHTML = "Checking that feed is valid..."; 
 
-  // let zinger = target.querySelector("small");
-  // zinger.classList.remove("hidden");
-  // setTimeout(() => {
-  //   zinger.classList.add("hidden")
-  // }, 2000);
-}
+      const response = await fetch('/test-feed', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+      });
 
-document.querySelectorAll(".copy-block").forEach((el) => {
-  let adminAddress = el.querySelector(".copy-target");
-  if (adminAddress) {
-    adminAddress.addEventListener("click", (event) => {
-      copy(el);
-    });
-    el.querySelector("a").addEventListener("click", () => { copy(el); });
-  }  
-});
+      if ( response.status == 404 ) {
+        console.log("No feed there");
+        messageDest.innerHTML = "Sorry, we couldn't find a valid feed at that URL";
+      } else if ( response.status > 200 ) {
+        console.log("Something went wrong");
+        messageDest.innerHTML = "Sorry, something went wrong";
+      } else {
+        const data = await response.json();
+        console.log(data);
 
+        if ( data.error ) {
+          messageDest.innerHTML = data.error;  
+        } else {
+          document.querySelector(".add-feed input[name='url']").value = data.url;
+          document.querySelector(".add-feed").attributes.action = "/feed";
+    
+          button.removeEventListener("click", checkForValidFeed);
+          button.type = "submit";
+    
+          messageDest.innerHTML = "Looks good! Click 'Add feed' one more time to create the account";  
+        }
+      }
+    };
 
-//
-// feed deletion confirmation dialog handler
-//
-let deleteConfirm = document.querySelector(".delete-confirm");
-if ( deleteConfirm ) {
-  let dialog = document.querySelector("#delete-confirmation");
-  function showModal() {
-    dialog.showModal();
+    button.addEventListener("click", checkForValidFeed);
   }
 
-  deleteConfirm.addEventListener("click", showModal);
-  dialog.addEventListener('close', () => {
-    if (dialog.returnValue == "default") {
-      deleteConfirm.removeEventListener("click", showModal);
-      deleteConfirm.type = "submit";
-      deleteConfirm.click();
-    }
+  function copy(target) {
+    target.querySelector(".copy-target").select();
+    document.execCommand("copy");
+  }
+
+  document.querySelectorAll(".copy-block").forEach((el) => {
+    let adminAddress = el.querySelector(".copy-target");
+    if (adminAddress) {
+      adminAddress.addEventListener("click", (event) => {
+        copy(el);
+      });
+      el.querySelector("a").addEventListener("click", () => { copy(el); });
+    }  
   });
-}
+
+
+  //
+  // feed deletion confirmation dialog handler
+  //
+  let deleteConfirm = document.querySelector(".delete-confirm");
+  if ( deleteConfirm ) {
+    let deleteDialog = document.querySelector("#delete-confirmation");
+    function showModal() {
+      deleteDialog.showModal();
+    }
+
+    deleteConfirm.addEventListener("click", showModal);
+    deleteDialog.addEventListener('close', () => {
+      if (deleteDialog.returnValue == "default") {
+        deleteConfirm.removeEventListener("click", showModal);
+        deleteConfirm.type = "submit";
+        deleteConfirm.click();
+      }
+    });
+  }
+});
